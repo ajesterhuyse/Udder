ac108aa42293e1a19b0f60d331c2145168fc3e68
AJEsterhuyse <esterhuyse.andre@gmail.com>
2021-08-05 18:46:46 +0200

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Andre J Esterhuyse on 2021/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
